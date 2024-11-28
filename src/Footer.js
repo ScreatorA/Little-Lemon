d63c8040c32bbbaaa0583f87fdbde715a602c248
Sugirthan Arulsothy <sugirthan.arulsothy.programmer@gmail.com>
2024-11-28 20:30:15 +0100

    Filling Nav and Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,31 @@
 function Footer()
 {
     return(
-        <footer>Footer</footer>
+        <footer>
+            <ul>
+                Doormat Navigation
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Menu</a></li>
+                <li><a href="">Reservations</a></li>
+                <li><a href="">Order Online</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+
+            <ul>
+                Contact
+                <li><a href="">Adress</a></li>
+                <li><a href="">Phone Number</a></li>
+                <li><a href="">Email</a></li>
+            </ul>
+
+            <ul>
+                Social Media Links
+                <li><a href="">Adress</a></li>
+                <li><a href="">Phone Number</a></li>
+                <li><a href="">Email</a></li>
+            </ul>
+        </footer>
     )
 }
 export default Footer;
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,42 @@
-function Nav()
-{
-    return(
-        <nav>Navigation</nav>
-    )
+
+
+function Nav() {
+  return (
+    <nav>
+        <img src="./icons_assets/Logo.svg" alt="Little Lemon logo"/>
+      <ul>
+        <li>
+          <a href='' alt='Home'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='' alt='About'>
+            About
+          </a>
+        </li>
+        <li>
+          <a href='' alt='Menu'>
+            Menu
+          </a>
+        </li>
+        <li>
+          <a href='' alt='Reservations'>
+            Reservations
+          </a>
+        </li>
+        <li>
+          <a href='' alt='Order Online'>
+            Order Online
+          </a>
+        </li>
+        <li>
+          <a href='' alt='Login'>
+            Login
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 export default Nav;
